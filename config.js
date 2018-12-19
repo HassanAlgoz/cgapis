a54c4245b1294d8bb320ccd8b7849f6879f83149
Hassan Algoz <HassanAlgoz@gmail.com>
2018-12-19 13:42:49 +0300

    Formatter configuration moved to its file

--- config.js
+++ config.js
@@ -1,15 +1,8 @@
-const path = require('path');
+const path = require("path");
 const config = {
-    server_dir:    path.join(__dirname, '/server'),
-    client_dir:    path.join(__dirname, '/client'),
-    services_file:  path.join(__dirname, '/services.json'),
-    api_path:       '/api/v1',
-
-    prettier: {
-        parser:        'typescript',
-        tabWidth:      4,
-        trailingComma: 'all',
-        semi:          true,
-    },
+    server_dir:    path.join(__dirname, "/server"),
+    client_dir:    path.join(__dirname, "/client"),
+    services_file:  path.join(__dirname, "/api.json"),
+    api_path:       "/api/v1",
 };
 module.exports = config;
