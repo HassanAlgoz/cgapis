e12cce17664beb8627ce1702cddaa05cdc4efc54
Hassan Algoz <HassanAlgoz@gmail.com>
2018-12-27 23:30:12 +0300

    Update

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const fs = require("fs-extra");
 const path = require("path");
 
+const ticker = require("./ticker");
 const config = require("./config");
+const parser = require("./parser/parser");
+const formatter = require("./formatter/formatter")();
 const clientGenerator = require("./client-generator");
 const serverGenerator = require("./server-generator");
-const parser = require("./parser/parser");
-const ticker = require("./ticker");
+const generateTypes = require("./types-generator/types-generator");
 
 
 fs.ensureDirSync(path.join(config.server_dir, "/api"));
@@ -13,21 +15,24 @@ fs.ensureDirSync(config.client_dir);
 
 const spec = parser.parse(fs.readFileSync(config.api_spec_file));
 
+async function main() {
+    await clientGenerator.generateFiles({
+        spec:      spec,
+        outputDir: config.client_dir,
+    });
 
-clientGenerator.generateFiles(spec, {
-    outputDir:   config.client_dir,
-});
+    await serverGenerator.generateFiles({
+        spec:      spec,
+        outputDir: config.server_dir,
+        apiPath:   config.api_path,
+    });
 
+    fs.copyFileSync(
+        path.join(__dirname, "templates", "copies", "express-server.js"),
+        path.join(config.server_dir, "server.js")
+    );
 
-// serverGenerator.generateFiles(spec, {
-//     outputDir:   config.server_dir,
-//     apiPath:     config.api_path,
-// });
+    console.log("Done");
+}
+main();
 
-
-fs.copyFileSync(
-    path.join(__dirname, "templates", "copies", "express-server.js"),
-    path.join(config.server_dir, "server.js")
-);
-
-console.log("Done");
