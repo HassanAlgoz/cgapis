88b0948fdc8500ecd755ef42ded9f9b2392cb26e
Hassan Algoz <HassanAlgoz@gmail.com>
2018-12-27 23:28:44 +0300

    Use destructuring assignment in parameters. Yet typed.

--- client-generator.js
+++ client-generator.js
@@ -1,23 +1,23 @@
-const generateTypes = require("./types-generator/types-generator");
-const formatter = require("./formatter/formatter")();
 const fs = require("fs");
 const path = require("path");
+const generateTypes = require("./types-generator/types-generator");
+const formatter = require("./formatter/formatter")();
 
 const typesPrefix = "Types";
 const js = require("./templates/js")({typesPrefix});
 
 module.exports = {
 
-    async generateFiles(spec, {outputDir}) {
+    async generateFiles({
+        spec,
+        outputDir,
+    }) {
         const services = [];
         for(const serviceName in spec.services) {
             const service = spec.services[serviceName];
             const methods = [];
             for(const opName in service.ops) {
                 const op = service.ops[opName];
-                // Generate the Fetch Method
-                // Methods starting with "get" are sent as HTTP GET requests with the parameters in the querystring of the URL.
-                // Other methods are sent as HTTP POST requests with the parameters in the body.
                 methods.push(generateRequestMethod(serviceName, opName, op.req, op.res));
             }
             services.push({
@@ -26,17 +26,20 @@ module.exports = {
             });
         }
 
-        const tsCode = await generateTypes(spec.refs);
+        let tsCode = await generateTypes(spec.refs);
+        tsCode = formatter.format(`
+            // AUTO GENERATED
+        `) + "\n" + tsCode;
         fs.writeFileSync(path.join(outputDir, "/api-types.ts"), tsCode);
 
         const code = `
-            // Auto-generated: PLEASE DO NOT CHANGE FILE
+            // AUTO GENERATED
+            import { AxiosInstance } from 'axios';
             import * as ${typesPrefix} from "./api-types"
-
-            export default function ({axios}) {
+            
+            export default function (axios: AxiosInstance) {
                 return {
                     ${services.map(service => `
-                    // Auto-generated service "${service.serviceName}"
                     ${service.serviceName}: {
                         ${service.methods.join(",\n")}
                     }
@@ -51,7 +54,7 @@ module.exports = {
 function generateRequestMethod (serviceName, methodName, req, res) {
     const isGet = methodName.startsWith("get") || methodName.startsWith("find") || methodName.startsWith("list") || methodName.startsWith("fetch") || methodName.startsWith("search");
     return `
-    async ${methodName}(${js.keyTypePairs("req", req)}) : Promise<[${js.CST("res", res)}]> {
+    async ${methodName}({${js.CSP(req)}}:{${js.keyTypePairs("req", req)}}) : Promise<[${js.CST("res", res)}]> {
         try {
             const response = await axios({
                 url: "/${serviceName}/${methodName}",
@@ -60,10 +63,8 @@ function generateRequestMethod (serviceName, methodName, req, res) {
         : `method: "post", data: {${js.CSP(req)}},`
 }
             });
-            const res = response.data as {
-                ${js.keyTypePairs("res", res)}
-            };
-            return [res.status, res.users];
+            const {${js.CSP(res)}} = response.data
+            return [${js.CSP(res)}];
         } catch (err) {
             // Print a pretty error message
             console.error(\`${serviceName}Service.${methodName}(\${JSON.stringify({${js.CSP(req)}})}) error:\`, err);
