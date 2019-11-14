7acca1cc3c4daf6aae51391deda130db2386cd43
Hassan Algoz <HassanAlgoz@gmail.com>
2019-11-14 07:50:20 +0300

    Disabled code, for now

--- validate/validation.js
+++ validate/validation.js
@@ -1,39 +1,39 @@
-const Ajv = require("ajv");
+// const Ajv = require("ajv");
 
-const spec = require("../spec");
+// const spec = require("../spec");
 
-const ajv = new Ajv({
-    allErrors: true, // check all rules collecting all errors.
-    schemas:   [spec.schemas],
-});
+// const ajv = new Ajv({
+//     allErrors: true, // check all rules collecting all errors.
+//     schemas:   [spec.schemas],
+// });
 
-const reqSchema = spec.services.Course.Course.getList.req;
-const data = {
-    skip:    0,
-    limit:   "",
-    context: "",
-};
-console.log("schema:", reqSchema);
-console.log("data:", data);
+// const reqSchema = services.Course.Course.getList.req;
+// const data = {
+//     skip:    0,
+//     limit:   "",
+//     context: "",
+// };
+// console.log("schema:", reqSchema);
+// console.log("data:", data);
 
-const validate  = ajv.compile(reqSchema);
+// const validate  = ajv.compile(reqSchema);
 
-if (!validate(data)) {
-    const errMap = {};
-    // console.log(validate.errors);
-    for(const err of validate.errors) {
-        const dataPath = err.dataPath.substring(1);
-        if (err.keyword === "enum") {
-            const enumValues = err.params["allowedValues"].map(v => `"${v}"`).join(", ");
-            errMap[dataPath] = err.message + ": " + enumValues;
-            continue;
-        }
-        errMap[dataPath] = err.message;
-    }
-    console.log("errMap:", errMap);
-} else {
-    console.log("OK");
-}
+// if (!validate(data)) {
+//     const errMap = {};
+//     // console.log(validate.errors);
+//     for(const err of validate.errors) {
+//         const dataPath = err.dataPath.substring(1);
+//         if (err.keyword === "enum") {
+//             const enumValues = err.params["allowedValues"].map(v => `"${v}"`).join(", ");
+//             errMap[dataPath] = err.message + ": " + enumValues;
+//             continue;
+//         }
+//         errMap[dataPath] = err.message;
+//     }
+//     console.log("errMap:", errMap);
+// } else {
+//     console.log("OK");
+// }
 
 
 // const schema = {
